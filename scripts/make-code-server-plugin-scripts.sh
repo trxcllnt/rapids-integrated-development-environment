5cbe5ff94c6b7c766a9085278442918a814e865e
ptaylor <paul.e.taylor@me.com>
2022-04-06 23:12:21 -0700

    add code-server plugins

---
+++ scripts/make-code-server-plugin-scripts.sh
@@ -0,0 +1,150 @@
+#! /usr/bin/env bash
+
+set -Eeo pipefail
+
+cd $(dirname "$(realpath "$0")")/..;
+
+PLUGINS_DIR="compose/code-server/home/.plugins"
+BUILD_IMAGE="pauletaylor/rapids-ide:cpp-builder-cuda${CUDA_VERSION:-11.6.0}-ubuntu20.04"
+REPOS="rmm raft cudf cumlprims_mg cuml cugraph-ops cugraph cuspatial"
+
+get_env_var_name() {
+    name="$(echo $1 | tr '[:lower:]' '[:upper:]')";
+    echo "${name//\-/_}";
+}
+
+dir_envvars() {
+    VARS=""
+    for x in ${REPOS}; do
+        name="$(get_env_var_name $x)";
+        source_dir="\$HOME/rapids/$x";
+        cpp_source_dir="\$(cpp-source-dir-util --project=$x)";
+        cpp_binary_dir="\$(cpp-binary-dir-util --project=$x)";
+        VARS+="
+    if [[ -d \"$source_dir\" ]]; then
+        echo \"${name}_SOURCE_DIR=$source_dir\" >> \"\$tmp_env_file\";
+        echo \"${name}_CPP_SOURCE_DIR=$cpp_source_dir\" >> \"\$tmp_env_file\";
+        echo \"${name}_CPP_BINARY_DIR=$cpp_binary_dir\" >> \"\$tmp_env_file\";
+    fi\
+"
+    done
+    echo -e "$VARS"
+}
+
+get_volumes() {
+    VOLUMES="volumes=\"\";"
+    for x in ${REPOS}; do
+        volume=
+        if [[ $x == $1 ]]; then
+            volume="-v \$HOME/rapids/$x:/opt/rapids/$x "
+        else
+            volume="-v \$HOME/rapids/$x:\$HOME/rapids/$x:ro "
+        fi
+        VOLUMES+="
+    if [[ -d \"\$HOME/rapids/$x\" ]]; then volumes+=\"$volume\"; fi"
+    done
+    echo -e "$VOLUMES"
+}
+
+copy_inputs_cmd() {
+    name="$(get_env_var_name $1)";
+    echo "copy-inputs-util --project=\\\"$1\\\" --src=\\\"\\\$${name}_SOURCE_DIR\\\" --bin=\\\"\\\$${name}_CPP_BINARY_DIR\\\"";
+}
+
+copy_output_cmd() {
+    name="$(get_env_var_name $1)";
+    echo "copy-output-util --project=\\\"$1\\\" --src=\\\"\\\$${name}_CPP_SOURCE_DIR\\\" --bin=\\\"\\\$${name}_CPP_BINARY_DIR\\\"";
+}
+
+make_script() {
+    cat << EOF > "$PLUGINS_DIR/$1-cpp/$2"
+#! /usr/bin/env bash
+set -Eeo pipefail;
+cd "\$(dirname "\$(realpath "\$0")")";
+if [[ -d "\$HOME/rapids/$1" ]]; then
+
+    tmp_env_file="\$(mktemp)";
+    touch "\$HOME/rapids/.env";
+    cp "\$HOME/rapids/.env" "\$tmp_env_file";
+
+    on_exit() {
+        ERRCODE="\$?";
+        rm -f "\$tmp_env_file" >/dev/null 2>&1 || true;
+        exit "\$ERRCODE";
+    }
+
+    trap on_exit ERR EXIT HUP INT QUIT TERM STOP PWR;
+
+    set -a && . "\$tmp_env_file" && set +a;
+
+    cpp_bin_dir="\$(cpp-binary-dir-util --project=$1)";
+    cpp_src_dir="\$(cpp-source-dir-util --project=$1)";
+    $(dir_envvars)
+
+    $(get_volumes $1)
+
+    docker run \\
+        --rm -it --runtime nvidia \\
+        --env-file "\$tmp_env_file" \\
+        \${volumes} \\
+        ${BUILD_IMAGE} \\
+        bash -c "${@:3}";
+
+    if [[ -f "\$cpp_bin_dir/compile_commands.json" ]]; then
+        ln -sf "\$cpp_bin_dir/compile_commands.json" "\$cpp_src_dir/compile_commands.json";
+    fi
+fi
+EOF
+
+    chmod +x "$PLUGINS_DIR/$1-cpp/$2"
+}
+
+make_clean_script() {
+    make_script $1 clean "clean-$1-cpp"
+}
+
+make_build_script() {
+    make_script $1 build "
+        $(copy_inputs_cmd $1) \\
+     && clean-$1-cpp \\
+     && build-$1-cpp \\
+     && $(copy_output_cmd $1)\
+";
+}
+
+make_compile_script() {
+    make_script $1 compile "
+        $(copy_inputs_cmd $1) \\
+     && compile-$1-cpp \\
+     && $(copy_output_cmd $1)\
+";
+}
+
+make_configure_script() {
+    make_script $1 configure "
+        $(copy_inputs_cmd $1) \\
+     && clean-$1-cpp \\
+     && configure-$1-cpp \\
+     && $(copy_output_cmd $1)\
+";
+}
+
+for x in ${REPOS}; do
+    echo "clean-$x-cpp";
+    make_clean_script "$x";
+done
+
+for x in ${REPOS}; do
+    echo "build-$x-cpp";
+    make_build_script "$x";
+done
+
+for x in ${REPOS}; do
+    echo "compile-$x-cpp";
+    make_compile_script "$x";
+done
+
+for x in ${REPOS}; do
+    echo "configure-$x-cpp";
+    make_configure_script "$x";
+done
