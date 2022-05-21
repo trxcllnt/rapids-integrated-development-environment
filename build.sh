215b19183d12c43cc62180d6c9fe7c7ff1937af7
ptaylor <paul.e.taylor@me.com>
2022-05-20 23:28:42 -0700

    define more build images, plugins

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -Eeo pipefail
 
 docker compose build --force-rm --pull     01-base
 docker compose build --force-rm --parallel 02-cpp 02-mamba
-docker compose build --force-rm            03-python
-docker compose build --force-rm            04-code-server
+docker compose build --force-rm --parallel 03-python 03-cudf-cpp 03-ucx-cpp
+docker compose build --force-rm --parallel 04-code-server 04-cudf-python 04-ucx-python
 
-docker system prune -f
+# docker system prune -f
--- configure-rmm.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -Eeo pipefail
-
-base_image="pauletaylor/rapids-ide:gcc${GCC_VERSION:-9}-cuda${CUDA_VERSION_MAJOR:-11}.${CUDA_VERSION_MINOR:-6}.${CUDA_VERSION_PATCH:-2}-${LINUX_DISTRO:-ubuntu20.04}"
-
-run-in-docker-build --repo=rmm -- /opt/rapids/plugins/rmm/cpp/configure
